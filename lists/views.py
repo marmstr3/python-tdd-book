0133480813290b869d78a79884746731f56862b7
Michael Armstrong <marmstr3@asu.edu>
2020-08-08 14:11:55 -0600

    Refactored home_page view to only create an object if the request is a POST request.

--- lists/views.py
+++ lists/views.py
@@ -4,8 +4,10 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
-
-    return render(request, 'home.html', {'new_item_text': item.text})
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+    
+    return render(request, 'home.html', {'new_item_text': new_item_text})
